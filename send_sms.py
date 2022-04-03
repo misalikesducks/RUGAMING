cb3cb3496f3340af259c679c6745bdcbb6d889f3
Connie Chen <conne.chen1101@users.noreply.github.com>
2022-04-03 00:27:25 -0400

    Co-authored-by: Brian-d-Wang <Brian-d-Wang@users.noreply.github.com> Co-authored-by: herniboi <herniboi@users.noreply.github.com>

--- send_sms.py
+++ send_sms.py
@@ -21,14 +21,14 @@ def sms_greeting():
     resp = MessagingResponse()
     body = re.sub(r'[^a-zA-Z0-9 ]', '', body)
     body = body.lower()
-    try:
-        ind = body.index("im")
+    ind = body.rfind("im")
+    if(ind != -1): 
         meme = body[ind + 3:len(body)]
         resp.message("Hi " + meme + ", I'm dad")
         return str(resp)
-    except:
+    else:
         resp.message(sms_reply(body))
-        return str(resp)
+    return str(resp)
 
 def sms_reply(game_message):
     """Send a dynamic reply to an incoming text message"""
@@ -49,6 +49,7 @@ def sms_reply(game_message):
     # Start our TwiML response
     #resp = MessagingResponse()
     price, location, link = scraping(body)
+    scrape2("adventure")
     wordy = " on sale for: "
     stra = title + wordy + price + " at: " + location + " " + link 
     # Determine the right reply for this message
@@ -59,10 +60,12 @@ def sms_reply(game_message):
 def scraping(game_name):
 
     #s = Service(r'C:/Users/cool4/Downloads/chromedriver_win32')
-    driver = webdriver.Chrome(executable_path=r'C:/Users/cool4/Downloads/chromedriver_win32/chromedriver.exe')
+    #driver = webdriver.Chrome(executable_path=r'C:/Users/cool4/Downloads/chromedriver_win32/chromedriver.exe')
+    s = Service('/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=s)
 
     driver.get('https://isthereanydeal.com/#/filter:&search/%s'%game_name)
-    time.sleep(1) 
+    time.sleep(2) 
     SCROLL_PAUSE_TIME = 0.4
 
 
@@ -87,28 +90,51 @@ def scraping(game_name):
 
     #page = requests.get('https://isthereanydeal.com') # Getting page HTML through request
     soup = BeautifulSoup(pagesource, 'lxml') # Parsing content using beautifulsoup
-    a = soup.select("a[href^=?game_name]")
-    print(a)
+    #a = soup.select("a[href^=?game_name]")
+    #ssprint(a)
     price = soup.find('a', {'data-evt' : '["shop","click","%s"]'%(game_name)})
     if (price == None):
         return
-    else:
-        try: 
-            rest = price.find_all_next('a', {'data-evt' : '["shop","click","%s"]'%(game_name)})
-            place = rest[len(rest)-1]['data-slc']
-            link = rest[len(rest)-1]['href']
-            return rest[len(rest)-1].text, place , link
-        except: 
-            rest = price
-            place = rest['data-slc']
-            link = rest['href']
-            return(rest.text, place , link)
+    
+    try: 
+        rest = price.find_all_next('a', {'data-evt' : '["shop","click","%s"]'%(game_name)})
+        place = rest[len(rest)-1]['data-slc']
+        link = rest[len(rest)-1]['href']
+        return rest[len(rest)-1].text, place , link
+    except: 
+        rest = price
+        place = rest['data-slc']
+        link = rest['href']
+        return(rest.text, place , link)
 
-# def scrape2(genre):
-#     page = requests.get('https://www.metacritic.com/browse/games/genre/metascore/%s/all?view=detailed'%genre)
-#     soup = BeautifulSoup(page.content, 'html.parser')
-      
-#     #games = soup.find(')
+def scrape2(genre):
+    s = Service('/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=s)
+
+    driver.get('https://www.metacritic.com/browse/games/genre/metascore/%s/all?view=detailed'%genre)
+    time.sleep(2) 
+    SCROLL_PAUSE_TIME = 0.4
+
+
+    # print('https://www.metacritic.com/browse/games/genre/metascore/%s/all?view=detailed'%genre)
+    # page = requests.get('https://www.metacritic.com/browse/games/genre/metascore/%s/all?view=detailed'%genre)
+    # print('https://www.metacritic.com/browse/games/genre/metascore/%s/all?view=detailed'%genre)
+    # soup = BeautifulSoup(page.content, 'html.parser')
+        
+    pagesource = driver.page_source
+    soup = BeautifulSoup(pagesource, 'lxml')
+
+    all = soup.find_all('span', {'class' : 'title numbered'})
+    top_three = []
+    for stuff in all: 
+        if(stuff.text.strip() == '1.'): 
+            top_three.append(stuff.find_next_sibling("a").h3.text)
+        elif(stuff.text.strip() == '2.'):
+            top_three.append(stuff.find_next_sibling("a").h3.text)
+        elif(stuff.text.strip() == '3.'):
+            top_three.append(stuff.find_next_sibling("a").h3.text)
+    return top_three
+
 
 if __name__ == "__main__":
     app.run(debug=True)
